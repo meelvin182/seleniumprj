1f3a7f2ef221e77b004749a3efc13f936bbb16fe
Danila <meelvin182@yandex.ru>
2018-09-24 22:09:48 +0300

    init

---
+++ src/main/java/ru/sokolov/Main.java
@@ -0,0 +1,37 @@
+package ru.sokolov;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import ru.stqa.selenium.factory.WebDriverPool;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Degrod\\Desktop\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.navigate().to("https://rosreestr.ru/wps/portal/p/cc_present/ir_egrn");
+       // TimeUnit.SECONDS.sleep(5);
+        List<WebElement> list = new ArrayList<>();
+        while (list.size() != 5) {
+            list.addAll(driver.findElements(By.className("v-textfield")));
+        }
+        System.out.println(list.size());
+        System.out.println(list);
+        list.get(0).click();
+        list.get(0).sendKeys("f5939ffe");
+        TimeUnit.SECONDS.sleep(1);
+        list.get(1).sendKeys("f955");
+        TimeUnit.SECONDS.sleep(1);
+        list.get(2).sendKeys("421a");
+        TimeUnit.SECONDS.sleep(1);
+        list.get(3).sendKeys("b30b");
+        TimeUnit.SECONDS.sleep(1);
+        list.get(4).sendKeys("884a5c527803");
+        driver.findElement(By.className("normalButton")).click();
+    }
+}
