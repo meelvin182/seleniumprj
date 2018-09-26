130dcf30f144142cfce9fa960c44ddbe75d56075
Xysch <30449080+Xysch@users.noreply.github.com>
2018-09-26 18:33:36 +0300

    Pages entities added (second one is incomplete)

--- src/main/java/ru/sokolov/Main.java
+++ src/main/java/ru/sokolov/Main.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import ru.sokolov.model.pages.SearchObjectsPage;
+import ru.sokolov.model.pages.SecondPage;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,7 +17,7 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
        // ClassLoader classLoader = getClass().getClassLoader();
         //File file = new File(classLoader.getResource("chromedriver.exe").getFile());
-        System.setProperty("webdriver.chrome.driver", "classes/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "src/resources/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.navigate().to("https://rosreestr.ru/wps/portal/p/cc_present/ir_egrn");
        // TimeUnit.SECONDS.sleep(5);
@@ -36,5 +38,14 @@ public class Main {
         TimeUnit.SECONDS.sleep(1);
         list.get(4).sendKeys("884a5c527803");
         driver.findElement(By.className("normalButton")).click();
+        TimeUnit.SECONDS.sleep(1);
+
+        SecondPage page = new SecondPage(driver);
+        page.openSearchParams();
+
+        TimeUnit.SECONDS.sleep(2);
+        SearchObjectsPage page1 = new SearchObjectsPage(driver);
+        page1.setCadastreNum("1232132132");
+        page1.setStreetName("QWEQWEQWEQWEQEW");
     }
 }
