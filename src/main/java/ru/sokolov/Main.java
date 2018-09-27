bc0c9486428834f7cf0cc9d9f4ff28cf3a71ea59
Kirill <kirill_17@list.ru>
2018-09-27 21:29:56 +0300

    Selection of regions added

--- src/main/java/ru/sokolov/Main.java
+++ src/main/java/ru/sokolov/Main.java
@@ -8,12 +8,20 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import ru.sokolov.model.pages.SearchObjectsPage;
 import ru.sokolov.model.pages.SecondPage;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
 @Deprecated
 public class Main {
+
+    private static List<String> regions = loadRegions();
+
+
     public static void main(String[] args) throws InterruptedException {
        // ClassLoader classLoader = getClass().getClassLoader();
         //File file = new File(classLoader.getResource("chromedriver.exe").getFile());
@@ -41,11 +49,32 @@ public class Main {
         TimeUnit.SECONDS.sleep(1);
 
         SecondPage page = new SecondPage(driver);
+        TimeUnit.MILLISECONDS.sleep(250);
         page.openSearchParams();
 
         TimeUnit.SECONDS.sleep(2);
         SearchObjectsPage page1 = new SearchObjectsPage(driver);
-        page1.setCadastreNum("1232132132");
+        page1.setCadastreNums("1232132132");
         page1.setStreetName("QWEQWEQWEQWEQEW");
+        page1.setRegion(regions.get(34));
+    }
+
+
+    //CTRL CV TO TEST
+    @SuppressWarnings("all")
+    private static List<String> loadRegions() {
+        List<String> values = new ArrayList<>();
+        try {
+            InputStream in = Main.class.getResourceAsStream("/regions.txt");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+            Scanner scanner = new Scanner(reader);       // create scanner to read
+            while (scanner.hasNextLine()) {  // while there is a next line
+                values.add(scanner.nextLine());
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+            System.exit(1);
+        }
+        return values;
     }
 }
--- src/main/java/ru/sokolov/model/pages/SearchObjectsPage.java
+++ src/main/java/ru/sokolov/model/pages/SearchObjectsPage.java
@@ -4,18 +4,25 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import java.util.Iterator;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class SearchObjectsPage {
 
     public final static String CADASTRE_AND_STREET_ELEMENTS_CLASS =  "v-textfield-prompt";
 
     private WebDriver driver;
-    private WebElement cadastreNum;
+
+    private WebElement cadastreNums;
     private WebElement streetName;
+    private WebElement region;
+    private WebElement findButton;
 
     public SearchObjectsPage(WebDriver driver) {
         this.driver = driver;
+        driver.findElements(By.className("v-button-caption"))
+                .forEach(s -> {if (s.getText().equals("Найти"))findButton = s;});
         setTextFieldElements(driver);
     }
 
@@ -23,19 +30,56 @@ public class SearchObjectsPage {
         List<WebElement> elementList = driver.findElements(By.className(CADASTRE_AND_STREET_ELEMENTS_CLASS));
         int maxWidth = Integer.parseInt(elementList.get(0).getAttribute("style").replaceAll("[\\D.]", ""));
         if(maxWidth > Integer.parseInt(elementList.get(1).getAttribute("style").replaceAll("[\\D.]", ""))){
-            cadastreNum = elementList.get(0);
+            cadastreNums = elementList.get(0);
             streetName = elementList.get(1);
         } else {
-            cadastreNum = elementList.get(1);
+            cadastreNums = elementList.get(1);
             streetName = elementList.get(0);
         }
     }
 
-    public void setCadastreNum(String cadastreNumVal){
-        cadastreNum.sendKeys(cadastreNumVal);
+    public void setCadastreNums(String cadastreNumVal){
+        cadastreNums.sendKeys(cadastreNumVal);
     }
 
     public void setStreetName(String streetNameVal){
         streetName.sendKeys(streetNameVal);
+    }
+
+    public WebElement getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) throws InterruptedException{
+        WebElement dropdownButton = driver.findElements(By.className("v-filterselect-button")).get(0);
+        dropdownButton.click();
+        TimeUnit.MILLISECONDS.sleep(500);
+        WebElement table = driver.findElement(By.className("v-filterselect-suggestmenu"));
+        selectDropdown(table, region);
+        this.region = driver.findElement(By.className("v-filterselect-input"));
+    }
+
+    private void selectDropdown(WebElement table, String value){
+        WebElement next = driver.findElement(By.xpath("//span[contains(text(), 'Next')]"));
+        List<WebElement> tableRows = table.findElement(By.tagName("table")).findElements(By.tagName("tr"));
+        Iterator iterator = tableRows.iterator();
+        while (iterator.hasNext()){
+            WebElement element = (WebElement) iterator.next();
+            if(element.getText().equals(value)){
+                element.click();
+                return;
+            }
+        }
+        try{
+            next.click();
+            TimeUnit.MILLISECONDS.sleep(500);
+        } catch (Exception e){
+            return;
+        }
+        selectDropdown(table, value);
+    }
+
+    private void pushFind(){
+        findButton.click();
     }
 }
