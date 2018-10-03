df56045987307b11ef6864e58e700528399514ce
Kirill <kirill_17@list.ru>
2018-10-03 20:18:56 +0300

    send coords button added

--- src/main/java/ru/sokolov/gui/MainScreen.java
+++ src/main/java/ru/sokolov/gui/MainScreen.java
@@ -1,11 +1,15 @@
 package ru.sokolov.gui;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.input.KeyCode;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import ru.sokolov.CoreKernelSupaClazz;
 
 
 public class MainScreen extends Application {
@@ -15,9 +19,14 @@ public class MainScreen extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        HBox hBox = new HBox();
         StackPane layout = new StackPane();
         Button testButton = new TestButton(primaryStage);
-        layout.getChildren().add(testButton);
+        Button sendCoords = new Button();
+        hBox.getChildren().addAll(testButton, sendCoords);
+        sendCoords.setOnAction(event -> CoreKernelSupaClazz.peformPut("test", "test"));
+        sendCoords.setText("Отправить координаты");
+        layout.getChildren().add(hBox);
         Scene scene = new Scene(layout, width, height);
 
         scene.setOnKeyPressed(event -> {
