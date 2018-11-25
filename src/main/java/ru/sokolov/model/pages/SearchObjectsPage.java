514aa4fa08fdbac494f6ff03a444c467a630c4ef
Xysch <30449080+Xysch@users.noreply.github.com>
2018-11-25 23:58:24 +0300

    Captcha solver added

--- src/main/java/ru/sokolov/model/pages/SearchObjectsPage.java
+++ src/main/java/ru/sokolov/model/pages/SearchObjectsPage.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ru.sokolov.model.entities.RequestEntity;
